41e245e7f08378d7fd2463614130eac3b90ff7f7
divya1107 <66174373+divya1107@users.noreply.github.com>
2025-10-07 12:58:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,15 @@ resource "aws_instance" "my_frst_ec2_instance" {
   tags = {
     Name = "ec2_instance_terfm"
   }
+
+}
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = "t3.micro"
+
+  tags = {
+    Name        = local.service_name
+    Owner       = local.owner
+    Environment = local.environment
+  }
 }
